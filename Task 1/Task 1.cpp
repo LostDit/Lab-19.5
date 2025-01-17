e52e3128c938b38845500f1cb4050d54f6b09fb0
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2025-01-17 10:26:54 +0300

    -Фикс новой функции -Исправил ошибку с поиском слова Remind (при нивелировании пунктуации не считались слова и при переносе на след строку если стоял любой знак пунктуации в конце)

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -2,17 +2,27 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <cctype>
+#include <regex>
 
 using namespace std;
 
 // Функция для приведения к нижнему регистру и удаление пунктуации
-string toLower(const string& str) {
+string preprocess(const string& str) {
     string result = str;
     transform(result.begin(), result.end(), result.begin(), [](unsigned char c) {
         return tolower(c);
         });
+    result.erase(remove_if(result.begin(), result.end(), [](unsigned char c) {
+        return ispunct(c) && c != '.'; // Сохр точки
+        }), result.end());
     return result;
+}
+
+// Подсчет количества вхождений слова в строке
+int countOccurrences(const string& line, const string& wordToFind) {
+    string cleanedLine = preprocess(line);
+    regex wordRegex("\\b" + wordToFind + "\\b");
+    return distance(sregex_iterator(cleanedLine.begin(), cleanedLine.end(), wordRegex), sregex_iterator());
 }
 
 int main() {
@@ -20,45 +30,20 @@ int main() {
 
     ifstream file("words.txt");
     if (!file.is_open()) {
-        cerr << "Не удалось открыть файл words.txt" << endl;
+        cerr << "Не удалось открыть файл words.txt." << endl;
         return 1;
     }
 
     string wordToFind;
     cout << "Введите слово для поиска: ";
     cin >> wordToFind;
+    wordToFind = preprocess(wordToFind);
 
-    // Привожу искомое слово к нижнему регистру
-    wordToFind = toLower(wordToFind);
-
-    string word;
-    int count = 0;
-
-    // Проверка слов из файла (возможно лучше было бы вынести в доп функцию)
-    while (file >> word) {
-        // Привожу слово из файла к нижнему регистру
-        word = toLower(word);
-
-        // Убираю пунктуацию в конце слов
-        word.erase(remove_if(word.begin(), word.end(), [](unsigned char c) {
-            return ispunct(c);
-            }), word.end());
-
-        // Сравнение с заданным словом
-        if (word == wordToFind) {
-            count++;
-        }
+    int totalCount = 0;
+    for (string line; getline(file, line); ) {
+        totalCount += countOccurrences(line, wordToFind);
     }
 
-    file.close();
-
-    // Вывожу результат пользу
-    if (count > 0) {
-        cout << "Слово \"" << wordToFind << "\" встречается " << count << " раз(а)." << endl;
-    }
-    else {
-        cout << "Слово \"" << wordToFind << "\" не найдено." << endl;
-    }
-
+    cout << "Слово \"" << wordToFind << "\" встречается " << totalCount << " раз(а)." << endl;
     return 0;
 }
